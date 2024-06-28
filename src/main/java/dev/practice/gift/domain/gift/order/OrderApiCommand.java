80a77458c92ada68a39efc285c05b561cc8a8e4a
pie <yejinh_h@naver.com>
2024-06-28 17:24:36 +0900

    #3 : OrderApiCaller 생성 - api 통신으로 e-commerce의 order에 주문 정보를 저장하는 역할

---
+++ src/main/java/dev/practice/gift/domain/gift/order/OrderApiCommand.java
@@ -0,0 +1,48 @@
+package dev.practice.gift.domain.gift.order;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+public class OrderApiCommand {
+
+	@Getter
+	@Builder
+	@ToString
+	public static class Register {
+		private final Long buyerUserId;
+		private final String payMethod;
+		private final List<RegisterOrderItem> orderItemList;
+	}
+
+	@Getter
+	@Builder
+	@ToString
+	public static class RegisterOrderItem {
+		private final String orderCount;
+		private final String itemToken;
+		private final String itemName;
+		private final String itemPrice;
+		private final List<RegisterOrderItemOptionGroup> orderItemOptionGroupList;
+	}
+
+	@Getter
+	@Builder
+	@ToString
+	public static class RegisterOrderItemOptionGroup {
+		private final Integer ordering;
+		private final String itemOptionGroupName;
+		private final List<RegisterOrderItemOption> orderItemOptionList;
+	}
+
+	@Getter
+	@Builder
+	@ToString
+	public static class RegisterOrderItemOption {
+		private final Integer ordering;
+		private final String itemOptionName;
+		private final Long itemOptionPrice;
+	}
+}
---
+++ src/main/java/dev/practice/gift/infrastructure/gift/order/OrderApiCallerImpl.java
@@ -0,0 +1,25 @@
+package dev.practice.gift.infrastructure.gift.order;
+
+import org.springframework.stereotype.Component;
+
+import dev.practice.gift.common.response.CommonResponse;
+import dev.practice.gift.domain.gift.order.OrderApiCaller;
+import dev.practice.gift.domain.gift.order.OrderApiCommand;
+import dev.practice.gift.infrastructure.retrofit.RetrofitUtils;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class OrderApiCallerImpl implements OrderApiCaller {
+	private final RetrofitUtils retrofitUtils;
+	private final RetrofitOrderApi retrofitOrderApi;
+
+	@Override
+	public String registerGiftOrder(OrderApiCommand.Register request) {
+		var call = retrofitOrderApi.registerOrder(request);
+		return retrofitUtils.responseSync(call)
+			.map(CommonResponse::getData)
+			.map(RetrofitOrderApiResponse.Register::getOrderToken)
+			.orElseThrow(RuntimeException::new);
+	}
+}
