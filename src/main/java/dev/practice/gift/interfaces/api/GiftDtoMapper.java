6350a8a02a37ce2ae848e1f94b4c6cf30ab768ef
pie <yejinh_h@naver.com>
2024-07-02 14:54:11 +0900

    #5 : DTO, DTO Mapper 생성

---
+++ src/main/java/dev/practice/gift/interfaces/api/GiftDtoMapper.java
@@ -0,0 +1,22 @@
+package dev.practice.gift.interfaces.api;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import dev.practice.gift.domain.gift.GiftCommand;
+
+@Mapper(
+	componentModel = "spring",
+	injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+	unmappedTargetPolicy = ReportingPolicy.ERROR
+)
+public interface GiftDtoMapper {
+	GiftCommand.Register of(GiftDto.RegisterRequest request);
+
+	GiftCommand.RegisterOrderItem of(GiftDto.RegisterOrderItemRequest request);
+
+	GiftCommand.RegisterOrderItemOptionGroup of(GiftDto.RegisterOrderItemOptionGroupRequest request);
+
+	GiftCommand.RegisterOrderItemOption of(GiftDto.RegisterOrderItemOptionRequest request);
+}
